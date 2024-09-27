79dbd60e916219c4cc15b72543c996c05da10817
Victor Zammit <victorzammit@icloud.com>
2024-09-27 10:53:58 +0200

    refactor UnitTests directory and improve documentation

---
+++ qa-backend-code-challenge/src/Betsson.OnlineWallets.Tests/UnitTests/DepositTest.cs
@@ -0,0 +1,19 @@
+using Betsson.OnlineWallets.Models;
+
+namespace Betsson.OnlineWallets.UnitTests;
+
+public class DepositTests {
+
+    [Fact]
+    public void Deposit_Amount_CanBeSetAndRetrieved() {
+        
+        // Create a new object of type deposit
+        var deposit = new Deposit();
+
+        // Setting deposit amount to any valid quantity
+        deposit.Amount = 150;
+
+        // Checking that the correct quantity is stored in the deposit amount
+        Assert.Equal(150, deposit.Amount);
+    }
+}
---
+++ qa-backend-code-challenge/src/Betsson.OnlineWallets.Tests/UnitTests/WithdrawalTest.cs
@@ -0,0 +1,19 @@
+using Betsson.OnlineWallets.Models;
+
+namespace Betsson.OnlineWallets.UnitTests;
+
+public class WithdrawalTests
+{
+    [Fact]
+    public void Withdrawal_Amount_CanBeSetAndRetrieved()
+    {
+        // Create a new object of type Withdrawal
+        var withdrawal = new Withdrawal();
+
+        // Setting withdrawal amount to any valid value
+        withdrawal.Amount = 30;
+
+        // Checking that the correct value is stored in the withdrawal amount
+        Assert.Equal(30, withdrawal.Amount);
+    }
+}
