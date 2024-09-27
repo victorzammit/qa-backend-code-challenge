f9264082dfa39a0d389c9487048c671f2555218d
Victor Zammit <victorzammit@icloud.com>
2024-09-27 15:04:50 +0200

    Add API test case to attempt a withdraw post command in the case where there are insufficient funds in balance

--- qa-backend-code-challenge/src/Betsson.OnlineWallets.Tests/ApiTests/ApiTest.cs
+++ qa-backend-code-challenge/src/Betsson.OnlineWallets.Tests/ApiTests/ApiTest.cs
@@ -1,6 +1,9 @@
 using AutoMapper;
 
+using Betsson.OnlineWallets.Exceptions;
+using Betsson.OnlineWallets.Models;
 using Betsson.OnlineWallets.Services;
+
 using Betsson.OnlineWallets.Web;
 using Betsson.OnlineWallets.Web.Mappers;
 using Betsson.OnlineWallets.Web.Models;
@@ -41,6 +44,28 @@ public class OnlineWalletApiTests : IClassFixture<WebApplicationFactory<Startup>
         var response = await _client.PostAsync("/OnlineWallet/Deposit", content);
 
         // Check that appropriate bad request response is received.
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task PostWithdrawal_InsufficientBalance_ReturnsBadRequest()
+    {
+        // Set up a new withdrawal request exceeding available balance in online wallet
+        var withdrawalRequest = new WithdrawalRequest {Amount = 1000};
+        var withdrawal = _mapper.Map<Withdrawal>(withdrawalRequest);
+
+        // Setting up lambda function to withdraw the funds specified in the withdrawal request.
+        // Configure to throw Insufficient balance exception.
+        _onlineWalletServiceMock.Setup(s => s.WithdrawFundsAsync(withdrawal))
+            .ThrowsAsync(new InsufficientBalanceException("Insufficient funds available"));
+
+        var content = new StringContent(
+            JsonConvert.SerializeObject(withdrawalRequest), Encoding.UTF8, "application/json");
+
+        // Send post request to withdraw from wallet
+        var response = await _client.PostAsync("/OnlineWallet/Withdraw", content);
+
+        // Evaluate response - should fail due to insufficient funds
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 }
