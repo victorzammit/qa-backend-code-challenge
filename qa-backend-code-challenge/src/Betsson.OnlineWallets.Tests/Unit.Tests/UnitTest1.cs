8479a3c7c5b17c7bb7271b6f8a700c861352a573
Victor Zammit <victorzammit@icloud.com>
2024-09-27 10:30:31 +0200

    refactor automatically generated test into Unit.Tests directory

---
+++ qa-backend-code-challenge/src/Betsson.OnlineWallets.Tests/Unit.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace Betsson.OnlineWallets.UnitTests;
+
+public class SetupTest
+{
+    [Fact]
+    public void SetupTest1()
+    {
+        // This is an automatically generated test which does not test any of the microservice
+        // functionality. It can be used to verify that the tests solution has been correctly
+        // built.
+    }
+}
