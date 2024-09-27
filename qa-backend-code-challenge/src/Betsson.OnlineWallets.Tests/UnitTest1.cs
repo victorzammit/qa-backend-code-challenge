4177145ab409e356ec425a46fc1aa5069d1d0725
Victor Zammit <victorzammit@icloud.com>
2024-09-27 10:21:34 +0200

    rename default test and add information docstring

--- qa-backend-code-challenge/src/Betsson.OnlineWallets.Tests/UnitTest1.cs
+++ qa-backend-code-challenge/src/Betsson.OnlineWallets.Tests/UnitTest1.cs
@@ -1,10 +1,12 @@
-namespace Betsson.OnlineWallets.Tests;
+namespace Betsson.OnlineWallets.UnitTests;
 
-public class UnitTest1
+public class SetupTest
 {
     [Fact]
-    public void Test1()
+    public void SetupTest1()
     {
-
+        // This is an automatically generated test which does not test any of the microservice
+        // functionality. It can be used to verify that the tests solution has been correctly
+        // built.
     }
 }
