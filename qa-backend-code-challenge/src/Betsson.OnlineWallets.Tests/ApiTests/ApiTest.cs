5340e5556c84c752e129879ede95ace697193ddb
Victor Zammit <victorzammit@icloud.com>
2024-09-27 15:01:53 +0200

    Add test to verify Deposit Post command in the case of an invalid value

--- qa-backend-code-challenge/src/Betsson.OnlineWallets.Tests/ApiTests/ApiTest.cs
+++ qa-backend-code-challenge/src/Betsson.OnlineWallets.Tests/ApiTests/ApiTest.cs
@@ -1,9 +1,15 @@
+using AutoMapper;
+
 using Betsson.OnlineWallets.Services;
 using Betsson.OnlineWallets.Web;
 using Betsson.OnlineWallets.Web.Mappers;
-using AutoMapper;
+using Betsson.OnlineWallets.Web.Models;
+
 using Moq;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System.Text;
+using System.Net;
 
 namespace Betsson.OnlineWallets.Tests.ApiTests;
 
@@ -21,5 +27,20 @@ public class OnlineWalletApiTests : IClassFixture<WebApplicationFactory<Startup>
         // AutoMapper setup using OnlineWalletMappingProfile
         var mapperConfig = new MapperConfiguration(cfg => cfg.AddProfile(new OnlineWalletMappingProfile()));
         _mapper = mapperConfig.CreateMapper();
+    }
+
+        [Fact]
+    public async Task PostDeposit_InvalidDepositRequest_ReturnsBadRequest() {
+        
+        // Create a new invalid Deposit request. Convert to Json format
+        var depositRequest = new DepositRequest {Amount = -100};
+        var content = new StringContent(
+            JsonConvert.SerializeObject(depositRequest), Encoding.UTF8, "application/json");
+
+        // Send Post deposit request
+        var response = await _client.PostAsync("/OnlineWallet/Deposit", content);
+
+        // Check that appropriate bad request response is received.
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 }
